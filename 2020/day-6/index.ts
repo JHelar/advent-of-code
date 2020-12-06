efbe0fd57a81c1f17c2e1692ecc618f0da6320d4
JHelar <john.helarsson@gmail.com>
2020-12-06 09:06:26 +0100

    Day 6 done

---
+++ 2020/day-6/index.ts
@@ -0,0 +1,38 @@
+import { readFileByLine } from "../utils"
+
+const readGroups = async () => {
+    const groups = []
+    let currentGroup = {}
+    let memberCount = 0
+    await readFileByLine('day-6/input.txt', (line) => {
+        if(line.length) {
+            const questions = line.split('')
+            for (const question of questions) {
+                if(!(question in currentGroup)) {
+                    currentGroup[question] = 0
+                }
+                currentGroup[question] += 1
+            }
+            memberCount += 1
+        } else {
+            groups.push({...currentGroup, memberCount })
+            currentGroup = {}
+            memberCount = 0
+        }
+    })
+    groups.push({...currentGroup, memberCount })
+    return groups
+}
+
+export default async () => {
+   const groups = await readGroups()
+   return groups.reduce((sum, { memberCount, ...questions }) => {
+       let groupSum = 0
+        for (const count of Object.values(questions)) {
+            if(count === memberCount) {
+                groupSum += 1
+            }
+        }
+        return sum + groupSum
+   }, 0)
+}
--- 2020/days.ts
+++ 2020/days.ts
@@ -3,11 +3,13 @@ import day2 from './day-2'
 import day3 from './day-3'
 import day4 from './day-4'
 import day5 from './day-5'
+import day6 from './day-6'
 
 export default {
     day1,
     day2,
     day3,
     day4,
-    day5
+    day5,
+    day6
 }
