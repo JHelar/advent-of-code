0d251fc7d7772f51b43112e120295c1e3a4cf51e
JHelar <john.helarsson@gmail.com>
2020-12-16 21:22:07 +0100

    Day 16 done

--- 2020/day-16/index.ts
+++ 2020/day-16/index.ts
@@ -1,18 +1,16 @@
-import { readlinkSync } from 'fs'
-import { type } from 'os'
 import { readFileByLine } from '../utils'
-
+ 
 type Ticket = Array<{ val: number, rules: string[] }>
 interface Rule {
   name: string
   minRange: number[]
   maxRange: number[]
 }
-
-
+ 
+ 
 const readInput = async (path: string) => {
   const rules: Array<Rule> = []
-  const nearbyTickets: Array<Ticket> = [] 
+  const nearbyTickets: Array<Ticket> = []
   let myTicket: Ticket = []
   let readState = 0
   await readFileByLine(path, (line) => {
@@ -35,7 +33,7 @@ const readInput = async (path: string) => {
             rules: valid
           }
         })
-  
+ 
         myTicket = values
       }
     } else if (readState === 2) {
@@ -49,39 +47,78 @@ const readInput = async (path: string) => {
             rules: valid
           }
         }, {} as Ticket)
-
+ 
         if(isValid) {
           nearbyTickets.push(values)
         }
       }
     }
   })
-
+ 
   return {
     rules,
     nearbyTickets,
     myTicket
   }
 }
-
+ 
 const isFieldValid = (v: number, { minRange, maxRange }: Rule) => {
   const [minMin, minMax] = minRange
   const [maxMin, maxMax] = maxRange
-
+ 
   const min = v >= minMin && v <= minMax
   const max = v >= maxMin && v <= maxMax
-
+ 
   return min || max
 }
-
+ 
+const findRuleFor = (valueIndex: number, takenRules: Array<string>, myTicket: Ticket, ruleMap: Record<string, Set<number>>): null | {index: number, rule: string}[] => {
+  if(valueIndex === myTicket.length) {
+    return []
+  }
+ 
+  const { rules } = myTicket[valueIndex]
+ 
+  const lookupRules = rules.filter(r => !takenRules.includes(r))
+  if(!lookupRules.length) return null
+ 
+  for (const rule of lookupRules) {
+    const isValidRule = ruleMap[rule].has(valueIndex)
+ 
+    if(isValidRule) {
+      const assumptions = findRuleFor(valueIndex + 1, [...takenRules, rule], myTicket, ruleMap)
+      if(assumptions) {
+        return [...assumptions, { index: valueIndex, rule }]
+      }
+    }
+  }
+ 
+  return null
+}
+ 
 export default async () => {
   const {
     myTicket,
     nearbyTickets,
     rules
   } = await readInput('day-16/input.txt')
-  
-  const lookupTickets = nearbyTickets.map((ticket, i) => ({ ticket, index: i })).filter(({ ticket }) => ticket.some(({ rules }) => rules.some(r => r.startsWith('departure'))))
-  console.log(nearbyTickets.length)
-  console.log(lookupTickets.length)
+  const ruleMap = myTicket.reduce((acc, { val, rules }, i) => {
+    const validRules = rules.filter(rule => nearbyTickets.every(nt => nt[i].rules.includes(rule)))
+    validRules.forEach(rule => {
+      if(!(rule in acc)) {
+        acc[rule] = new Set<number>()
+      }
+      acc[rule].add(i)
+    })
+    return acc
+  }, {} as Record<string, Set<number>>)
+ 
+  const result = findRuleFor(0, [], myTicket, ruleMap)
+  console.log(result)
+  return result?.reduce((sum, { index, rule }) => {
+    if(rule.startsWith('departure')) {
+      sum *= myTicket[index].val
+    }
+    return sum
+  },1)
 }
