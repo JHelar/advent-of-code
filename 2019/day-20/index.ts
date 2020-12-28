71e55398a2932b3788d10c9b05d20c7238d6d96c
John Larsson <john.larsson@klarna.com>
2020-12-28 16:50:18 +0100

    Continue 2019

---
+++ 2019/day-20/index.ts
@@ -0,0 +1,110 @@
+import fs from 'fs'
+import { Portal, Node, getNodeKey } from './node'
+import { aStar } from './aStar'
+
+const labelPerRow: Record<number, { name: string[], rowIndex: number, colIndex: number }[]> = {}
+const labelPerColumn: Record<number, { name: string[], rowIndex: number, colIndex: number }[]> = {}
+const stringMap = fs.readFileSync('day-20/test.txt').toString().split('\n').map((row, rowIndex) => {
+    const column: string[] = []
+    row.split('').forEach((c, cIndex) => {
+        if(c === '.' || c === '#') {
+            column.push(c)
+        } else if(c !== ' ') {
+            if(!(cIndex in labelPerColumn)) {
+                labelPerColumn[cIndex] = []
+            }
+            
+            const colLabel = labelPerColumn[cIndex].find(c => c.rowIndex + 1 === rowIndex)
+            if(colLabel) {
+                colLabel.name.push(c)
+            } else {
+                labelPerColumn[cIndex].push({
+                    name: [c],
+                    rowIndex: rowIndex,
+                    colIndex: cIndex,
+                })
+            }
+
+            if(!(rowIndex in labelPerRow)) {
+                labelPerRow[rowIndex] = []
+            }
+            const rowLabel = labelPerRow[rowIndex].find(r => r.colIndex + 1 === cIndex)
+            if(rowLabel) {
+                rowLabel.name.push(c)
+            } else {
+                labelPerRow[rowIndex].push({
+                    name: [c],
+                    colIndex: cIndex,
+                    rowIndex
+                })
+            }
+        }
+    })
+    return column
+}).filter(row => row.length)
+
+const labels = [...Object.values(labelPerColumn).flatMap((labels) => labels.map(({ name, colIndex, rowIndex }) => ({
+    name: name.join(''),
+    x: colIndex - 2,
+    y: rowIndex 
+}))),...Object.values(labelPerRow).flatMap((labels) => labels.map(({ name, colIndex, rowIndex }) => ({
+    name: name.join(''),
+    x: colIndex,
+    y: rowIndex - 2 
+})))].filter(({ name }) => name.length === 2)
+
+const map = stringMap.reduce((map, row, rowIndex) => {
+    row.forEach((col, columnIndex) => {
+        const label = labels.find((l) => l.x === columnIndex && l.y === rowIndex)
+        if(label) {
+            const node = new Portal(columnIndex, rowIndex, label.name)
+            map[node.getKey()] = node
+        } else {
+            const node = new Node(columnIndex, rowIndex, col)
+            map[node.getKey()] = node
+        }
+    })
+    return map
+}, {} as Record<string, Node>)
+
+const nodes = Object.values(map).map((node, ni, nodes) => {
+    node.neighbours = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
+    ].map(([x, y]) => map[getNodeKey(node.x + x, node.y + y)]).filter(n => n && n.road)
+    if(node instanceof Portal) {
+        const portalEnd = nodes.find(n => n instanceof Portal && n.name === node.name && n.getKey() !== node.getKey())
+        if(portalEnd) {
+            [
+                [portalEnd.x, portalEnd.y + 1],
+                [portalEnd.x, portalEnd.y - 1],
+                [portalEnd.x + 1, portalEnd.y],
+                [portalEnd.x - 1, portalEnd.y]
+            ]
+            .map(([x, y]) => map[getNodeKey(node.x + x, node.y + y)])
+            .filter(n => n && n.road)
+            .forEach(n => node.neighbours.push(n))
+        }
+    }
+    return node
+}).filter(node => node.road)
+
+const path = aStar(nodes)
+let count = 0
+if(path) {
+    let lookAt = path.prev
+    while(lookAt) {
+        count++
+        lookAt = lookAt.prev
+    }
+}
+// console.log({
+//     count
+// })
+nodes.forEach(n => {
+    if(n instanceof Portal) {
+        console.log(n.toString())
+    }
+})
---
+++ 2019/intcode/index.ts
@@ -0,0 +1,259 @@
+const OP_CODES = {
+    HALT: 99,
+    ADD: 1,
+    MUL: 2,
+    INPUT: 3,
+    OUTPUT: 4,
+    JMP_TRUE: 5,
+    JMP_FALSE: 6,
+    LE: 7,
+    EQ: 8,
+    ADJ: 9
+}
+
+const OP_CODE_PARAMS = {
+    [OP_CODES.ADD]: 3,
+    [OP_CODES.MUL]: 3,
+    [OP_CODES.INPUT]: 1,
+    [OP_CODES.OUTPUT]: 1,
+    [OP_CODES.HALT]: 0,
+    [OP_CODES.JMP_TRUE]: 2,
+    [OP_CODES.JMP_FALSE]: 2,
+    [OP_CODES.LE]: 3,
+    [OP_CODES.EQ]: 3,
+    [OP_CODES.ADJ]: 1
+}
+
+class Operator {
+    program: IntProgram
+
+    constructor(program: IntProgram) {
+        this.program = program
+    }
+
+    add() {
+        const one = this.program.getValueForParam(0, this.program.cursor + 1n)
+        const two = this.program.getValueForParam(1, this.program.cursor + 2n)
+
+        const result = one + two
+        this.program.setValueForParam(2, result, this.program.cursor + 3n)
+        this.program.advanceCursor()
+    }
+
+    mul() {
+        const one = this.program.getValueForParam(0, this.program.cursor + 1n)
+        const two = this.program.getValueForParam(1, this.program.cursor + 2n)
+
+        const result = one * two
+        this.program.setValueForParam(2, result, this.program.cursor + 3n)
+        this.program.advanceCursor()
+    }
+
+    input(value: bigint) {
+        this.program.setValueForParam(0, value, this.program.cursor + 1n)
+        this.program.advanceCursor()
+    }
+
+    output() {
+        const value = this.program.getValueForParam(0, this.program.cursor + 1n)
+        this.program.advanceCursor()
+
+        return value
+    }
+
+    jumpIfTrue() {
+        const value = this.program.getValueForParam(0, this.program.cursor + 1n)
+        if(value !== 0n) {
+            this.program.cursor = this.program.getValueForParam(1, this.program.cursor + 2n)
+        } else {
+            this.program.advanceCursor()
+        }
+    }
+
+    jumpIfFalse() {
+        const value = this.program.getValueForParam(0, this.program.cursor + 1n)
+        if(value === 0n) {
+            this.program.cursor = this.program.getValueForParam(1, this.program.cursor + 2n)
+        } else {
+            this.program.advanceCursor()
+        }
+    }
+
+    lesserThan() {
+        const one = this.program.getValueForParam(0, this.program.cursor + 1n)
+        const two = this.program.getValueForParam(1, this.program.cursor + 2n)
+
+        if(one < two) {
+            this.program.setValueForParam(2, 1n, this.program.cursor + 3n)
+        } else {
+            this.program.setValueForParam(2, 0n, this.program.cursor + 3n)
+        }
+        this.program.advanceCursor()
+    }
+
+    equals() {
+        const one = this.program.getValueForParam(0, this.program.cursor + 1n)
+        const two = this.program.getValueForParam(1, this.program.cursor + 2n)
+
+        if(one === two) {
+            this.program.setValueForParam(2, 1n, this.program.cursor + 3n)
+        } else {
+            this.program.setValueForParam(2, 0n, this.program.cursor + 3n)
+        }
+        this.program.advanceCursor()
+    }
+
+    adjust() {
+        const value = this.program.getValueForParam(0, this.program.cursor + 1n)
+        this.program.relativeBase += value
+        this.program.advanceCursor()
+    }
+}
+
+export class IntProgram {
+    memory: Map<bigint, bigint>
+    originInput: string
+    cursor: bigint
+    relativeBase: bigint
+    running: boolean
+    opCode: number
+    paramModes: number[]
+    operator: Operator
+
+    constructor(input: string) {
+        this.originInput = input
+        this.memory = new Map()
+        this.reset()
+        this.cursor = 0n
+        this.relativeBase = 0n
+        this.running = true
+        this.opCode = -1
+        this.paramModes = []
+        this.operator = new Operator(this)
+    }
+
+    getValueAt(address: bigint){
+        if(address < 0) throw new Error(`Tried to get value from invalid address: ${address}, opcode: ${this.opCode}, cursorAt: ${this.cursor}, paramModes: ${this.paramModes}`)
+        if(!this.memory.has(address)) {
+            this.memory.set(address, 0n)
+            return 0n
+        }
+        return this.memory.get(address)!
+    }
+
+    getValueFromPointerAt(address: bigint) {
+        const pointerAddress = this.getValueAt(address)
+        return this.getValueAt(pointerAddress)
+    }
+
+    getValueRelativeFromPointerAt(address: bigint) {
+        const relativeAddress = this.getValueAt(address)
+        return this.getValueAt(this.relativeBase + relativeAddress)
+    }
+
+    setValueAt(value: bigint, address: bigint) {
+        this.memory.set(address, value)
+    }
+
+    setValueFromPointerAt(value: bigint, address: bigint) {
+        const saveAtAddress = this.getValueAt(address)
+        this.setValueAt(value, saveAtAddress)
+    }
+
+    setValueRelativeFromPointerAt(value: bigint, address: bigint) {
+        const relativeAddress = this.getValueAt(address)
+        this.setValueAt(value, this.relativeBase + relativeAddress)
+    }
+
+    getValueForParam(paramNo: number, address: bigint){
+        const mode = this.paramModes[paramNo] || 0
+        if(mode === 0) {
+            return this.getValueFromPointerAt(address)
+        } else if(mode === 2) {
+            return this.getValueRelativeFromPointerAt(address)
+        }
+        return this.getValueAt(address)
+    }
+
+    setValueForParam(paramNo: number, value: bigint, address: bigint) {
+        const mode = this.paramModes[paramNo] || 0
+        if(mode === 0) {
+            return this.setValueFromPointerAt(value, address)
+        } else if(mode === 2) {
+            return this.setValueRelativeFromPointerAt(value, address)
+        }
+        return this.setValueAt(value, address)
+    }
+
+    advanceCursor() {
+        this.cursor = this.cursor + 1n + BigInt(this.paramModes.length)
+    }
+
+    advanceProgram() {
+        const code = this.getValueAt(this.cursor).toString()
+        this.opCode = Number(code.substr(code.length - 2))
+        this.paramModes = code.padStart(5, '0')
+            .substr(0, 3)
+            .split('')
+            .map(Number)
+            .reverse()
+            .slice(0, OP_CODE_PARAMS[this.opCode])
+    }
+
+    reset() {
+        this.initializeMemory()
+        this.cursor = 0n
+        this.relativeBase = 0n
+        this.running = true
+        this.opCode = -1
+        this.paramModes = []
+    }
+
+    initializeMemory() {
+        this.memory = new Map()
+        this.originInput.split(',').map(i => BigInt(i)).forEach((i, index) => {
+            this.memory.set(BigInt(index), i)
+        })
+    }
+
+    exec(input: bigint[]) {
+        this.advanceProgram()
+        let result: number | bigint = Infinity
+        switch(this.opCode) {
+            case OP_CODES.ADD:
+                this.operator.add()
+                break;
+            case OP_CODES.MUL:
+                this.operator.mul()
+                break;
+            case OP_CODES.JMP_TRUE:
+                this.operator.jumpIfTrue()
+                break
+            case OP_CODES.JMP_FALSE:
+                this.operator.jumpIfFalse()
+                break
+            case OP_CODES.LE:
+                this.operator.lesserThan()
+                break
+            case OP_CODES.EQ:
+                this.operator.equals()
+                break
+            case OP_CODES.ADJ:
+                this.operator.adjust()
+                break
+            case OP_CODES.INPUT:
+                this.operator.input(input.shift()!)
+                break
+            case OP_CODES.OUTPUT:
+                result = this.operator.output()
+                break
+            case OP_CODES.HALT:
+                this.running = false
+                break;
+            default:
+                this.running = false
+                break
+        }
+        return result
+    }
+}
