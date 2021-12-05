a4ffead0412b772edb81513513c10ea6f07bce9f
JHelar <john.helarsson@gmail.com>
2021-12-05 12:43:03 +0100

    feat: day 5

---
+++ 2021/day-5/index.ts
@@ -0,0 +1,89 @@
+// INPUT URL: https://adventofcode.com/2021/day/5/input
+type Point = [number, number];
+type Range = [Point, Point];
+type Ranges = Array<Range>;
+type Tile = number;
+type SeaMap = Record<string, Tile>;
+
+const pointToKey = ([x, y]: Point) => `(${x},${y})`;
+
+const isVerticalLine = ([[x1], [x2]]: Range) => x1 === x2;
+const isHorizontalLine = ([[, y1], [, y2]]: Range) => y1 === y2;
+
+const getIsOnLine = (range: Range) => {
+  const [[x1, y1], [x2, y2]] = range;
+  if (isHorizontalLine(range)) return ([, y]: Point) => y === y1;
+  if (isVerticalLine(range)) return ([x]: Point) => x === x1;
+
+  const a = (x2 - x1) !== 0 ? (y2 - y1) / (x2 - x1) : 0;
+  const m = y1 - a * x1;
+  return ([x, y]: Point) => y === a * x + m;
+};
+
+const setRangeToMap = (map: SeaMap, range: Range) => {
+  const [[x1, y1], [x2, y2]] = range;
+  const isOnLine = getIsOnLine(range);
+
+  const xStart = Math.min(x1, x2);
+  const yStart = Math.min(y1, y2);
+
+  const xEnd = Math.max(x1, x2);
+  const yEnd = Math.max(y1, y2);
+
+  for (let y = yStart; y <= yEnd; y++) {
+    for (let x = xStart; x <= xEnd; x++) {
+      const point: Point = [x, y];
+
+      const mapKey = pointToKey(point);
+      if (isOnLine(point)) {
+        map[mapKey] = ++map[mapKey] || 1;
+      }
+    }
+  }
+};
+
+const getRanges = async () => {
+  const inputString = await Deno.readTextFile("./day-5/input.txt");
+  const ranges = inputString.split("\n").reduce((points, line) => {
+    const range = line.split(" -> ").map((pointStr) =>
+      pointStr.split(",").map(Number) as Point
+    );
+    return [...points, range as Range];
+  }, [] as Ranges);
+
+  return ranges;
+};
+
+export const part1 = async () => {
+  const ranges = await getRanges();
+  const map: SeaMap = {};
+
+  for (const range of ranges) {
+    if (isHorizontalLine(range) || isVerticalLine(range)) {
+      setRangeToMap(map, range);
+    }
+  }
+
+  const maxCount = Object.values(map).reduce(
+    (sum, value) => value > 1 ? sum + 1 : sum,
+    0,
+  );
+
+  return maxCount;
+};
+
+export const part2 = async () => {
+  const ranges = await getRanges();
+  const map: SeaMap = {};
+
+  for (const range of ranges) {
+    setRangeToMap(map, range);
+  }
+
+  const maxCount = Object.values(map).reduce(
+    (sum, value) => value > 1 ? sum + 1 : sum,
+    0,
+  );
+
+  return maxCount;
+};
--- 2021/days.ts
+++ 2021/days.ts
@@ -4,3 +4,5 @@ export * as day1 from "./day-1/index.ts";
 export * as day2 from "./day-2/index.ts";
 export * as day3 from "./day-3/index.ts";
 export * as day4 from "./day-4/index.ts";
+
+export * as day5 from './day-5/index.ts'
