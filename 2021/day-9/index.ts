bb52ba0bceba72ab37c20bdb65e0c70a9977804d
JHelar <john.helarsson@gmail.com>
2021-12-09 20:24:18 +0100

    feat: added rendering for day 9

--- 2021/day-9/index.ts
+++ 2021/day-9/index.ts
@@ -1,6 +1,15 @@
 // INPUT URL: https://adventofcode.com/2021/day/9/input
+import {
+  createRenderer,
+  drawPixel,
+  makeColor,
+  PALETTE,
+  renderToScreen,
+  sleep,
+} from "../renderer/render.ts";
 type Tile = {
   value: number;
+  position: Position;
   filled?: number;
 };
 type Row = Array<Tile>;
@@ -8,7 +17,6 @@ type Map = Array<Row>;
 type Position = [number, number];
 type Basin = {
   tile: Tile;
-  position: Position;
   size: number;
 };
 
@@ -17,11 +25,12 @@ const getMapTile = ([x, y]: Position, map: Map): Tile | undefined =>
 
 const getMap = async () => {
   const inputString = await Deno.readTextFile("./day-9/input.txt");
-  const map = inputString.split("\n").map((row) => {
+  const map = inputString.split("\n").map((row, y) => {
     const trimmed = row.trim();
     const splitted = trimmed.split("");
-    const casted = splitted.map((num) => ({
+    const casted = splitted.map((num, x) => ({
       value: parseInt(num, 10),
+      position: [x, y],
     }));
     return casted as Row;
   }) as Map;
@@ -52,8 +61,8 @@ const getBasins = (map: Map) => {
         basins.push({
           tile: {
             value,
+            position: [x, y],
           },
-          position: [x, y],
           size: 1,
         });
       }
@@ -67,8 +76,15 @@ const floodFillFrom = (
   [x, y]: Position,
   map: Map,
   fillValue: number,
+  order: Array<Tile> = [],
+  basinFirst = false,
 ): number => {
-  map[y][x].filled = fillValue;
+  if (basinFirst) {
+    map[y][x].filled = -1;
+  } else {
+    map[y][x].filled = fillValue;
+  }
+  order.push(map[y][x]);
 
   const topPos: Position = [x, y - 1];
   const bottomPos: Position = [x, y + 1];
@@ -81,7 +97,7 @@ const floodFillFrom = (
     if (
       tile !== undefined && tile.value < 9 && tile.filled === undefined
     ) {
-      filled += floodFillFrom(pos, map, fillValue);
+      filled += floodFillFrom(pos, map, fillValue, order);
     }
   }
   return filled;
@@ -101,10 +117,55 @@ export const part2 = async () => {
   const sizes: number[] = [];
   for (let i = 0; i < basins.length; i++) {
     const basin = basins[i];
-    const size = floodFillFrom(basin.position, map, i);
+    const size = floodFillFrom(basin.tile.position, map, i);
     sizes.push(size);
   }
 
   const topThree = sizes.sort((a, b) => b - a).slice(0, 3);
   return topThree.reduce((sum, size) => sum * size, 1);
+};
+
+export const render = async () => {
+  const map = await getMap();
+  const basins = getBasins(map);
+
+  await createRenderer(100, 100);
+  for (const row of map) {
+    for (const tile of row) {
+      const [x, y] = tile.position;
+      const color = makeColor(
+        93 * (1 + (tile.value / 9)),
+        93 * (1 + (tile.value / 9)),
+        93 * (1 + (tile.value / 9)),
+      );
+      drawPixel(x, y, color);
+    }
+  }
+
+  await renderToScreen();
+  await sleep(50);
+  for (const { tile: { position: [x, y] } } of basins) {
+    // renderer.setCell(x, y, "X", PALETTE.YELLOW, PALETTE.BLUE_DARK);
+    drawPixel(x, y, PALETTE.YELLOW);
+    await renderToScreen();
+    await sleep(5);
+  }
+
+  for (const { tile: { position } } of basins) {
+    // renderer.setCell(x, y, "X", PALETTE.YELLOW, PALETTE.BLUE_DARK);
+    const order: Array<Tile> = [];
+    floodFillFrom(position, map, 0, order, true);
+    for (const tile of order) {
+      if (tile.filled !== undefined) {
+        const [x, y] = tile.position;
+        if (tile.filled === -1) {
+          drawPixel(x, y, PALETTE.YELLOW);
+        } else {
+          drawPixel(x, y, PALETTE.BLUE_LIGHT);
+        }
+        await renderToScreen();
+        await sleep(5);
+      }
+    }
+  }
 };
