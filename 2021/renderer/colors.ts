d3c1ac91d8fc138f3b43633eefbd8d57a3762d30
JHelar <john.helarsson@gmail.com>
2021-12-11 12:20:10 +0100

    fix: updated pallette

--- 2021/renderer/colors.ts
+++ 2021/renderer/colors.ts
@@ -1,8 +1,10 @@
 import {
+  Colors,
   createColor,
 } from "https://deno.land/x/terminal@0.1.0-dev.3/src/mod.ts";
 
 export const PALETTE = {
+  ...Colors,
   BLUE_DARK: createColor(15, 15, 35),
   BLUE_LIGHT: createColor(9, 126, 192),
   BROWN: createColor(150, 122, 60),
