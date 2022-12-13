678eb378ba1a94e0ec6151d7f4f648ea85b097dd
JHelar <john.helarsson@gmail.com>
2022-12-13 20:48:54 +0100

    feat: day 12 done

--- 2022/days/day-12/main.rs
+++ 2022/days/day-12/main.rs
@@ -37,7 +37,7 @@ struct Node {
 impl Node {
     fn is_neighbour(self: &Node, position: Position, map: &HeightMap) -> bool {
         if let Some(neighbour) = map.get(&position) {
-            return neighbour.elevation <= self.elevation || neighbour.elevation == self.elevation + 1;
+            return neighbour.elevation <= self.elevation + 1;
         }
         false
     }
@@ -73,7 +73,13 @@ fn parse_map() -> HeightMap {
                     map.insert(
                         position,
                         Node {
-                            elevation: if elevation == "S" { 'a' as u32 } else if elevation == "E" { 'z' as u32 } else { elevation.chars().next().unwrap() as u32 },
+                            elevation: if elevation == "S" {
+                                'a' as u32
+                            } else if elevation == "E" {
+                                'z' as u32
+                            } else {
+                                elevation.chars().next().unwrap() as u32
+                            },
                             node_type,
                             position,
                             distance,
@@ -114,22 +120,20 @@ fn get_neighbours(node_position: Position, map: &HeightMap) -> Vec<Position> {
     positions
 }
 
-fn find_path(map: &mut HeightMap) -> Option<Position> {
-    let start_node_position = map
-        .values()
-        .find(|node| matches!(node.node_type, NodeType::Start))
-        .unwrap()
-        .position;
-
+fn find_path(map: &mut HeightMap, start_node_position: Position) -> Option<Position> {
+    map.values_mut().for_each(|node| {
+        node.distance = if node.position == start_node_position { 0 } else { i32::MAX as i64 };
+        node.visited = false;
+        node.parent = None
+    });
+    
     let mut unvisited: Vec<Position> = vec![start_node_position];
 
     while let Some(current_position) = unvisited.pop() {
         let current_node = map.get(&current_position).unwrap();
-        println!("{:?}", current_position);
-        if matches!(current_node.node_type, NodeType::End) {
-            return Some(current_position);
+        if current_node.visited {
+            continue;
         }
-
         let current_distance = current_node.distance;
         let neighbours = get_neighbours(current_position, map);
 
@@ -140,6 +144,10 @@ fn find_path(map: &mut HeightMap) -> Option<Position> {
             if neighbour_node.distance >= neighbour_distance {
                 neighbour_node.distance = neighbour_distance;
                 neighbour_node.parent = Some(current_position);
+            }
+
+            if matches!(neighbour_node.node_type, NodeType::End) {
+                return  Some(neighbour_position);
             }
 
             if neighbour_node.visited == false {
@@ -159,18 +167,39 @@ fn find_path(map: &mut HeightMap) -> Option<Position> {
 
 fn part1() {
     let map = &mut parse_map();
-    let mut parent = find_path(map);
-    let mut steps = -1;
-    while let Some(node_position) = parent {
-        steps += 1;
-        println!("{:?}", node_position);
-        parent = map.get(&node_position).unwrap().parent
-    }
+    let start_node_position = map
+        .values()
+        .find(|node| matches!(node.node_type, NodeType::Start))
+        .unwrap()
+        .position;
+
+    let end_pos = find_path(map, start_node_position).unwrap();
+    let steps = map.get(&end_pos).unwrap().distance;
 
     println!("Steps: {}", steps);
 }
 
-fn part2() {}
+fn part2() {
+    let map = &mut parse_map();
+    let elevation = 'a' as u32;
+
+    let positions: Vec<Position> = map
+        .values()
+        .filter(|node| node.elevation == elevation)
+        .map(|node| node.position).collect();
+
+    let mut best = i32::MAX as i64;
+    for start_node_position in positions {
+        if let Some(end_pos) = find_path(map, start_node_position) {
+            let steps = map.get(&end_pos).unwrap().distance;
+            if steps < best {
+                best = steps;
+            }
+        }
+    }
+
+    println!("Steps: {}", best);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
