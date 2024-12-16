0f664c8e41e62cf2ff9876b9bda7ae6d1d16ea34
John Larsson <john.larsson@getbright.se>
2024-12-16 20:39:41 +0100

    fix: add some visualisation

--- 2024/days/16/main.rs
+++ 2024/days/16/main.rs
@@ -1,5 +1,6 @@
 use hashbrown::{HashMap, HashSet};
-use std::fmt::Display;
+use ratatui::{layout::Rect, style::Stylize, widgets::Paragraph, Frame};
+use std::{fmt::Display, thread::sleep, time::Duration};
 use vector2::{Vector2, DOWN, LEFT, RIGHT, UP, ZERO};
 
 mod vector2;
@@ -51,6 +52,36 @@ impl Maze {
     fn get_tile_mut(&mut self, position: &Vector2) -> Option<&mut Tile> {
         let index = self.get_tile_index(position);
         self.map.get_mut(index)
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        for y in 0..self.size.1 {
+            for x in 0..self.size.0 {
+                let paragraph = match self.get_tile(&Vector2(x, y)) {
+                    Some(tile) => match tile {
+                        Tile::Visited(_, direction) => match *direction {
+                            NORTH => Paragraph::new("^").white(),
+                            SOUTH => Paragraph::new("v").white(),
+                            EAST => Paragraph::new(">").white(),
+                            WEST => Paragraph::new("<").white(),
+                            ZERO => Paragraph::new("O").white(),
+                            _ => panic!("Bad visited"),
+                        },
+                        Tile::Reindeer(_) => Paragraph::new("@").red().bold(),
+                        Tile::Air(_) => Paragraph::new(" ").gray(),
+                        Tile::Start(_) => Paragraph::new("S").gray(),
+                        Tile::End(_) => Paragraph::new("E").gray(),
+                        Tile::Wall(_) => Paragraph::new("#").green(),
+                    },
+                    None => panic!("ooops"),
+                };
+                let rect = Rect::new(x as u16, y as u16, 1, 1);
+                if rect.x >= frame.area().width || rect.y >= frame.area().height {
+                  continue;
+                }
+                frame.render_widget(paragraph, rect);
+            }
+        }
     }
 }
 
@@ -112,29 +143,30 @@ fn read_input() -> Maze {
     Maze { map: tiles, size }
 }
 
-fn find_best_path(maze: &Maze) -> (usize, HashSet<Node>) {
+fn find_best_path(maze: &mut Maze) -> (usize, HashSet<Node>) {
+  let mut terminal = ratatui::init();
+    terminal.clear().unwrap();
+    
     let start_position = maze
         .map
         .iter()
         .find(|t| matches!(t, Tile::Start(_)))
         .unwrap()
-        .get_position();
+        .get_position()
+        .clone();
 
     let end_position = maze
         .map
         .iter()
         .find(|t| matches!(t, Tile::End(_)))
         .unwrap()
-        .get_position();
+        .get_position()
+        .clone();
 
     let mut costs: HashMap<Node, usize> = HashMap::new();
     costs.insert((start_position.clone(), EAST), 0);
 
-    let mut visit: Vec<(
-        Vector2,
-        Vector2,
-        HashMap<Node, Option<Node>>,
-    )> = Vec::new();
+    let mut visit: Vec<(Vector2, Vector2, HashMap<Node, Option<Node>>)> = Vec::new();
     visit.push((
         start_position.clone(),
         EAST,
@@ -148,14 +180,32 @@ fn find_best_path(maze: &Maze) -> (usize, HashSet<Node>) {
     while let Some((current_position, direction, mut previous)) = visit.pop() {
         let current_cost = costs.get(&(current_position, direction)).unwrap().clone();
 
-        if current_position == *end_position && current_cost <= best_cost {
+        if current_position == end_position && current_cost <= best_cost {
             best_cost = current_cost;
             let mut position = (current_position, direction);
             path.insert(position.clone());
 
+            let mut this_path = vec![position.clone()];
+
             while let Some(previous_position) = previous.get(&position).unwrap_or(&None) {
                 position = *previous_position;
-                path.insert(position);
+                if path.contains(&position) {
+                  continue;
+                }
+                this_path.push(position.clone());
+            }
+            this_path.reverse();
+
+            let mut previous = this_path.first().unwrap();
+            *maze.get_tile_mut(&previous.0).unwrap() = Tile::Reindeer(previous.0);
+
+            for position in &this_path[1..] {
+              path.insert(position.clone());
+              terminal.draw(|frame| maze.draw(frame)).unwrap();
+              sleep(Duration::from_millis(100));
+              *maze.get_tile_mut(&previous.0).unwrap() = Tile::Visited(previous.0, previous.1);
+              previous = position; 
+              *maze.get_tile_mut(&previous.0).unwrap() = Tile::Reindeer(previous.0);
             }
             continue;
         }
@@ -205,25 +255,25 @@ fn find_best_path(maze: &Maze) -> (usize, HashSet<Node>) {
         });
     }
 
+    ratatui::restore();
+
     (best_cost, path)
 }
 
 fn part1() -> Option<usize> {
-    let maze = read_input();
-    let (cost, _) = find_best_path(&maze);
+    let mut maze = read_input();
+    let (cost, _) = find_best_path(&mut maze);
     Some(cost)
 }
 
 fn part2() -> Option<usize> {
     let mut maze = read_input();
-    let (_, path) = find_best_path(&maze);
+    let (_, path) = find_best_path(&mut maze);
 
     let mut points = HashSet::new();
     for (position, _) in path.iter() {
         points.insert(*position);
-        *maze.get_tile_mut(position).unwrap() = Tile::Visited(*position, ZERO);
     }
-    println!("{maze}");
     Some(points.len())
 }
 
